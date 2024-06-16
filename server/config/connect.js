0c56e4ce0dd154b36fbe59143691747ce6b7be03
abdellah <admirablea42@gmail.com>
2024-06-16 22:35:30 +0200

    add .env file

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb://localhost:27017/user", {
+  .connect(process.env.BACKEND_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
